7e607065a7c3fb3c2a6a208a8e50ecffd5fed28b
Lucas Zluhan <lszluhan@hotmail.com>
2022-10-07 18:46:07 -0300

    refactor container to import cart single app & produtcs single app together

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,8 +1,12 @@
-import { mount } from 'products/ProductsIndex'
-import 'cart/CartShow'
+import { mount as productsMount } from 'products/ProductsIndex'
+import { mount as cartMount } from 'cart/CartShow'
 
 console.log('container!')
 
 const myProducts = document.querySelector('#my-products')
 
-if (myProducts) mount(myProducts)
+if (myProducts) productsMount(myProducts)
+
+const myCart = document.querySelector('#my-cart')
+
+if (myCart) cartMount(myCart)
